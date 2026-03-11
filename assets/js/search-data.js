7fc3012078eb6469710605bb1b5ad48c16ccb5f5
shaxuanmeng <89436429+shaxuanmeng@users.noreply.github.com>
2026-03-11 00:04:00 +0000

    Deploying to gh-pages from @ shaxuanmeng/shaxuanmeng.github.io@cb921535b4d3c128d3230ea13bc2c979726d478e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=ya_qj20AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
